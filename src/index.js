a29bcfb31652263a29877792e31901c46c3ce108
Barry Tielkes <barry@instantmagazine.com>
2017-08-31 22:30:03 +0200

    removed unused reduser and added example config

--- src/index.js
+++ src/index.js
@@ -8,22 +8,30 @@ import { createStore } from 'redux';
 import { Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import reducer from './reducer';
 import App from './App';
 import history from './history';
 import { config } from './firebaseConfig';
+/*
+// example:
+config = {
+	apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	authDomain: 'fb-db-name.firebaseapp.com',
+	databaseURL: 'https://fb-db-name.firebaseio.com',
+	projectId: 'fb-db-name',
+	storageBucket: 'fb-db-name.appspot.com',
+	messagingSenderId: '012345678910',
+};
+*/
 
 const rootReducer = combineReducers({
 	firebase: firebaseStateReducer,
-	main: reducer,
 });
-// const store = createStore(rootReducer);
 
 const createStoreWithFirebase = compose(
 	reactReduxFirebase(config, {}) //, { userProfile: 'users' })
 )(createStore);
 
-const initialState = { main: {}, firebase: {} };
+const initialState = { firebase: {} };
 
 // Create store with reducers and initial state
 const store = createStoreWithFirebase(rootReducer, initialState);
